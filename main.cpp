eb5babde36f950591c299d4ce94882ebe6cfb0ad
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-04-09 22:27:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include "game.h"
+#include "SDL_ttf.h"
 Game *game =nullptr;
 int main(int argc,char *argv[])
 {
     game = new Game();
+    TTF_Init();
+    //Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024);
     const int FPS = 60;
     const int frameDelay = 1000/FPS;
     Uint32 frameStart;
     int frameTime;
     static const int WINDOW_HEIGHT = 800;
     static const int WINDOW_WIDTH = 800;
-    //const int CELL_SIZE = 40;
-    game ->init("SDL Tutorial",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_WIDTH,WINDOW_HEIGHT,false);
+    game ->init("Obstacle",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_WIDTH,WINDOW_HEIGHT,false);
     while (game->running())
     {
-        frameStart = SDL_GetTicks();
+        //frameStart = SDL_GetTicks();
         game -> handleEvents();
         game ->update();
         game ->render();
