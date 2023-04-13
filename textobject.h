c408f8c680c0093f0456bf9f1723c372f300c49e
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-04-13 22:54:47 +0700

    Update textobject.h

--- textobject.h
+++ textobject.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "game.h"
+#include "GameObject.h"
 class Text
 {
 public:
     Text();
     ~Text();
     void RenderText(const char* file,int ptsize,SDL_Color color,std::string s,int x,int y);
+    void EndGame(int diem,int hiscore,const char* filename,SDL_Color color,GameObject* cityzen1,GameObject* cityzen2,Game* GAME);
 private:
 TTF_Font* font;
 };
