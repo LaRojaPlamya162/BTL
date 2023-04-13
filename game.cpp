ba86db4b81a68d13ac856148b844e398d0355f7e
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-04-13 22:53:21 +0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -5,6 +5,7 @@
 #include "audio.h"
 #include "highscore.h"
 #include "Map.h"
+#include "Menu.h"
 GameObject* player;
 GameObject* obstacle;
 Text* text;
@@ -12,7 +13,10 @@ TextureManager* border;
 SDL_Renderer* Game::renderer = nullptr;
 Map* map;
 Audio* audio;
+Menu* MenuPointer;
+Game* GAME = nullptr;
 HighscoreManager* highscoremanager ;
+TextureManager* background;
 SDL_Color color = {255,255,255};
 Game::Game()
 {}
@@ -40,31 +44,15 @@ void Game::init(const char *title,int xpos,int ypos,int width,int height,bool fu
             std::cout<<"Renderer created"<<std::endl;
         }
         isRunning = true;
+        finish=true;
         }
-
-        /*if (TTF_Init()==-1)
-        {
-            text->success = false;
-        }
-        else
-        {
-            text->success = true;
-        }
-        if (text->success = false)
-        {
-            std::cout <<"Intialize fonts failed"<<std::endl;
-        }
-        else
-        {
-            std::cout <<"Intialize completed"<<std::endl;
-        }*/
         player =  new GameObject("Car.png",WINDOW_WIDTH,560);
         obstacle = new GameObject("ObstacleCar.png",NULL,NULL);
         map = new Map();
         text = new Text();
         audio = new Audio();
-        //highscoremanager = new HighscoreManager();
-
+        highscoremanager = new HighscoreManager();
+        MenuPointer = new Menu();
     }
 void Game::handleEvents()
 {
@@ -85,12 +73,16 @@ void Game::handleEvents()
                     player->MoveDown = false;
                     player->MoveLeft = false;
                     player->MoveRight = false;
+                    MenuPointer->MovingUp=true;
+                    MenuPointer->MovingDown=false;
                     break;
                 case SDLK_DOWN:
                     player->MoveUp = false;
                     player->MoveDown = true;
                     player->MoveLeft = false;
                     player->MoveRight = false;
+                    MenuPointer->MovingUp=false;
+                    MenuPointer->MovingDown=true;
                     break;
                 case SDLK_LEFT:
                     player->MoveUp = false;
@@ -104,41 +96,48 @@ void Game::handleEvents()
                     player->MoveLeft = false;
                     player->MoveRight = true;
                     break;
+                case SDLK_RETURN:
+                    MenuPointer->choose = true;
                 default:
                     break;
         }
     }
+    if(player->MoveDown==true||player->MoveUp==true||player->MoveRight==true||player->MoveLeft==true)dem++;
+    //std::cout<<dem<<" ";
 }
 
 void Game::update()
 {
-    obstacle->ObstacleUpdate();
+    obstacle->ObstacleUpdate(player);
     player->PlayerUpdate();
-    diem++;
-    if (player->CheckCollision(player->PlayerGetRect(),obstacle->ObstacleGetRect()))
-    {
-        SDL_DestroyWindow(window);
-    }
-    /*if(highscoremanager->addHighscore(diem)&&!isRunning)
-    {
-        //std::cout << diem;
-    }
-*/
+    if(dem>0)diem++;
+}
+void Game::MenuRender()
+{
+    SDL_RenderClear(Game::renderer);
+    map ->DrawMap();
+    //border ->Appear(WINDOW_HEIGHT,WINDOW_WIDTH);
+    MenuPointer->RenderMenu(text);
+    MenuPointer->UpdateMenu(text,GAME,map);
+    //border->LoadImageFile("road.png",0,0,800,800);
+    border ->Appear(WINDOW_HEIGHT,WINDOW_WIDTH);
+    //background->LoadImageFile("road.png",0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+    SDL_RenderPresent(Game::renderer);
 }
 void Game::render()
 {
-
     std::string DIEM = std::to_string(diem);
     SDL_RenderClear(Game::renderer);
+    //background->LoadImageFile("road.png",0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
     map ->DrawMap();
     player->PlayerRender();
     obstacle->ObstacleRender();
     border ->Appear(WINDOW_HEIGHT,WINDOW_WIDTH);
     text->RenderText("OpenSans-Bold.ttf",50,color,DIEM,360,0);
+    text->EndGame(diem,highscoremanager->addHighscore(diem),"OpenSans-Bold.ttf",color,player,obstacle,GAME);
     //audio->OpenAudio("GameSound.wav");
     SDL_RenderPresent(Game::renderer);
     }
-
 void Game::clean()
 {
     SDL_DestroyWindow(window);
