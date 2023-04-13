2937416fe927a616a7463754c02cb2db1c0e4b85
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-04-13 22:54:00 +0700

    Update textobject.cpp

--- textobject.cpp
+++ textobject.cpp
@@ -19,72 +19,29 @@ void Text::RenderText(const char* file,int ptsize,SDL_Color color,std::string s,
     SDL_DestroyTexture(gText);
     SDL_FreeSurface(text);
 }
-/*TextObject::TextObject()
+void Text::EndGame(int diem,int highscore,const char* filename,SDL_Color color,GameObject* cityzen1,GameObject* cityzen2,Game* GAME)
 {
-    text_color_.r = 255;
-    text_color_.g = 255;
-    text_color_.b = 255;
-    texture_ = NULL;
+    std::string DIEM = std::to_string(diem);
+    if (diem==highscore&&cityzen1->Finish())
+    {
+        //SDL_RenderClear(Game::renderer);
+        SDL_Delay(2000);
+        RenderText(filename,50,color,"New Record",250,380);
+        RenderText(filename,40,color,DIEM,320,350);
+        SDL_RenderPresent(Game::renderer);
+        SDL_Delay(5000);
+        GAME->finish=true;
+    }
+    else if (diem<highscore&&cityzen1->CheckCollision(cityzen1->PlayerGetRect(),cityzen2->ObstacleGetRect()))
+    {
+        //SDL_RenderClear(Game::renderer);
+        SDL_Delay(2000);
+        //RenderText(filename,50,color,"New Record",380,380);
+        RenderText(filename,50,color,"Your Score",250,350);
+        RenderText(filename,40,color,DIEM,450,500);
+        SDL_RenderPresent(Game::renderer);
+        SDL_Delay(5000);
+        GAME->finish=true;
+    }
 }
-TextObject::~TextObject()
-{
 
-}
-bool TextObject::LoadFromRenderText(TTF_Font* font, SDL_Renderer* screen)
-{
-    SDL_Surface* text_surface = TTF_RenderText_Solid(font,str_val_.c_str(),text_color_,)
-    if (text_surface)
-    {
-        texture_=SDL_CreateTextureFromSurface(screen,text_surface);
-        width_=text_surface->w;
-        height_=text_surface->h;
-        SDL_FreeSurface(text_surface);
-
-    }
-    return texture!=NULL;
-}
-void TextObject::Free()
-{
-    if(texture!=NULL)
-    {
-        SDL_DestroyTexture(texture_);
-        texture_=NULL;
-    }
-
-}
-void TextObject::SetColor(Uint8 red,Uint8 greeen,Uint8 blue)
-{
-    text_color_.r = red;
-    text_color_.g = green;
-    text_color_.b = blue;
-
-}
-void TextObject::SetColor(int type)
-{
-    if(type==RED_TEXT)
-    {
-      SDL_Color color = {255,0,0};
-      text_color = color;
-    }
-    else if(type==WHITE_TEXT)
-    {
-        SDL_Color color = {255,255,255};
-        text_color = color;
-    }
-    else if(type==BLACK_TEXT)
-    {
-        SDL_Color color = {0,0,0};
-        text_color = color;
-    }
-}
-void TextObject::RenderText(SDL_Renderer* screen,int xp,int yp, SDL_Rect* clip = NULL, double angle =0.0,SDL_Point* center = NULL,SDL_RendererFlip flip = SDL_FLIP_NONE)
-{
-    SDL_Rect renderQuad = {x,y,width_,height_};
-    if (clip!=NULL)
-    {
-        renderQuad.w = clip->w;
-        renderQuad.h = clip->h;
-    }
-    SDL_RenderCopyEx(screen,texture_,clip,&RenderQuad,angle,center,flip);
-
-}*/
