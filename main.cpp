06984ca3b84421757fee805949f59594abc4a3ea
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-04-15 00:48:08 +0700

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main(int argc,char *argv[])
     game = new Game();
     TTF_Init();
     //Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024);
-    const int FPS = 60;
-    const int frameDelay = 1000/FPS;
+    //const int FPS = 60;
+    //const int frameDelay = 1000/FPS;
     Uint32 frameStart;
     int frameTime;
     static const int WINDOW_HEIGHT = 800;
@@ -15,29 +15,32 @@ int main(int argc,char *argv[])
     game ->init("Obstacle",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_WIDTH,WINDOW_HEIGHT,false);
     while(game->running()==true)
     {
-    while (game->finish==true)
+    while(game->menuInit()==false)
     {
         //frameStart = SDL_GetTicks();
-        game -> handleEvents();
+        //game -> handleEvents();
         //game ->update();
+        //std::cout << game->Menuinit<<std::endl;
         game->MenuRender();
+        //std::cout << game->Menuinit<<std::endl;
         //game ->render();
         //frameTime = SDL_GetTicks() - frameStart;
         //if (frameDelay > frameTime)
         //{
             //SDL_Delay(frameDelay - frameTime);
+        //std::cout <<"Loops"<<std::endl;
+        //break;
     }
-    while (game->finish==false)//&&game->MenuInit==true)
+    while(game->menuInit()==true)
     {
         game -> handleEvents();
         game ->update();
         //game->MenuRender();
         game ->render();
+    }
+    }
+       return 0;
+    }
 
-    }
-    }
-
-    return 0;
-    }
 
 
