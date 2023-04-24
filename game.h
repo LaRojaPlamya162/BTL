5b76cb5675a53cb2c788f7d7f784451c34699836
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-04-24 23:29:40 +0700

    Update game.h

--- game.h
+++ game.h
@@ -13,18 +13,32 @@ class Game
     void init(const char *title,int xpos,int ypos,int width,int height,bool fullscreen);
     void handleEvents();
     void update();
+    void MenuRender();
     void render();
     void clean();
+    void Restart();
     bool running(){return isRunning;};
+    bool menuInit(){return Menuinit;};
+    //void RUNNING();
     static SDL_Renderer* renderer;
+    SDL_Window *window;
     int diem=0;
     bool isRunning;
+    bool Menuinit;
+    bool MovingUp=false;
+    bool MovingDown=false;
+    bool choose=false;
+    int dem=0;
+    bool keyPressed=false;
     private:
-    SDL_Window *window;
+    //SDL_Window *window;
     SDL_Texture* gTexture;
     const int WINDOW_HEIGHT = 800;
     const int WINDOW_WIDTH = 800;
     const int CELL_SIZE = 40;
+    SDL_Rect MenuDestRect,MenuSourceRect;
+    SDL_Rect PointerSrcRect,PointerDestRect;
+    SDL_Texture* Pointer;
 };
 
 #endif // GAME_H
