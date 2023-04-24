59356f0f4cc30c3e63759500e5db22e90be02b06
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2023-04-24 23:52:29 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,36 @@
 #include "game.h"
+#include "textobject.h"
 #include "SDL_ttf.h"
 Game *game =nullptr;
+Text *gText = new Text();
 int main(int argc,char *argv[])
 {
     game = new Game();
+    //menu = new Menu();
     TTF_Init();
     //Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024);
     //const int FPS = 60;
     //const int frameDelay = 1000/FPS;
-    Uint32 frameStart;
-    int frameTime;
+    //Uint32 frameStart;
+    //int frameTime;
     static const int WINDOW_HEIGHT = 800;
     static const int WINDOW_WIDTH = 800;
     game ->init("Obstacle",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WINDOW_WIDTH,WINDOW_HEIGHT,false);
     while(game->running()==true)
     {
-    while(game->menuInit()==true)
+    if (game->Menuinit==true)
     {
+        game->handleEvents();
         game->MenuRender();
     }
-    while(game->menuInit()==false)
+    else
     {
+        game ->Restart();
         game -> handleEvents();
-        game ->update();
         game ->render();
+        game ->update();
     }
     }
-       return 0;
-    }
-
-
+    return 0;
+}
 
